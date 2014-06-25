242105409f8af805911f996b880cb3d5c330fdf2
Job van der Voort <jobvandervoort@gmail.com>
2014-06-25 15:38:06 +0200

    hide download instructions until OS chosen

--- source/javascripts/downloads.js
+++ source/javascripts/downloads.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   
   // Installation options
   $('#tabs .tab').hide();
-  $('#tabs .tab:first').show();
   $(".dropdown-menu li a").click(function(){
     var selText = $(this).text();
     $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
