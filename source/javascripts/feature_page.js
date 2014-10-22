88550ff194968793a63722d72615790320ecda35
Job van der Voort <jobvandervoort@gmail.com>
2014-10-22 14:49:46 +0200

    add stickynav and google analytics tracking links

--- source/javascripts/feature_page.js
+++ source/javascripts/feature_page.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   $('.image-link').magnificPopup({
     type:'image'
   });
+
+  var nav = $('.qnav');
+  $(window).scroll(function() {
+    var scroll = nav.offset().top - $(window).scrollTop();
+    if (scroll <= 50) {
+      nav.addClass("qfix");
+    }
+    if ($(window).scrollTop() <= 690) {
+      nav.removeClass("qfix");
+    }
+  });
 });
