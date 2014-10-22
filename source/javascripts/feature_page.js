2946cbcf05b1397c2f4d8fbece85ab37225357a1
Job van der Voort <jobvandervoort@gmail.com>
2014-10-22 16:03:31 +0200

    link to features page from products, improve js and css performance

--- source/javascripts/feature_page.js
+++ source/javascripts/feature_page.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
     type:'image'
   });
 
-  var nav = $('.qnav');
-  $(window).scroll(function() {
-    var scroll = nav.offset().top - $(window).scrollTop();
-    if (scroll <= 50) {
-      nav.addClass("qfix");
-    }
-    if ($(window).scrollTop() <= 690) {
-      nav.removeClass("qfix");
-    }
-  });
+  if ($('.qnav').is('#qnav')) {
+    var nav = $('.qnav');
+    $(window).scroll(function() {
+      var scroll = nav.offset().top - $(window).scrollTop();
+      if (scroll <= 50) {
+        nav.addClass("qfix");
+      }
+      if ($(window).scrollTop() <= 690) {
+        nav.removeClass("qfix");
+      }
+    });
+  }
 });
