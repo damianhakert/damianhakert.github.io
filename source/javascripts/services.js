b1591c19150261c8cca3a6abce0e6d9f12d01c42
Martijn <mvbemmel@hotmail.com>
2014-10-31 17:55:30 +0100

    Add icons to services page

---
+++ source/javascripts/services.js
@@ -0,0 +1,7 @@
+  $('.services #consultancy').addClass('animated zoomIn');
+  $('.services #development').addClass('animated zoomIn');
+  $('.services #gitlab-com').addClass('animated zoomIn');
+  $('.services #subscription').addClass('animated zoomIn');
+  $('.services #training').addClass('animated zoomIn');
+$(document).ready(function() {
+});
