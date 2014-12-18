cc15ca414f2d64b8f1829f3ef7a2455cacc669df
Job van der Voort <jobvandervoort@gmail.com>
2014-12-18 16:12:57 +0100

    improve headers feature page

--- source/javascripts/feature_page.js
+++ source/javascripts/feature_page.js
@@ -7,10 +7,12 @@ $(document).ready(function() {
     var nav = $('.qnav');
     $(window).scroll(function() {
       var scroll = nav.offset().top - $(window).scrollTop();
+      console.log(scroll);
+      console.log($(window).scrollTop());
       if (scroll <= 50) {
         nav.addClass("qfix");
       }
-      if ($(window).scrollTop() <= 690) {
+      if ($(window).scrollTop() <= 500) {
         nav.removeClass("qfix");
       }
     });
