c95a10c22915da5e1181ec8fb25ef7ba25c6370e
Job van der Voort <jobvandervoort@gmail.com>
2015-01-17 16:30:18 -0800

    always show second nav in features page

--- source/javascripts/feature_page.js
+++ source/javascripts/feature_page.js
@@ -2,17 +2,4 @@ $(document).ready(function() {
   $('.image-link').magnificPopup({
     type:'image'
   });
-
-  if ($('.qnav').is('#qnav')) {
-    var nav = $('.qnav');
-    $(window).scroll(function() {
-      var scroll = nav.offset().top - $(window).scrollTop();
-      if (scroll <= 50) {
-        nav.addClass("qnav2", 500);
-      }
-      if ($(window).scrollTop() <= 500) {
-        nav.removeClass("qnav2");
-      }
-    });
-  }
 });
