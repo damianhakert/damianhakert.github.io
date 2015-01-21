7f44e86578848380a94249b2c9ece11711a427a0
MarkoHipinspired <test@hipinspired.com>
2015-01-21 16:55:45 +0100

    changes from comments

--- source/javascripts/landing_slider.js
+++ source/javascripts/landing_slider.js
@@ -48,7 +48,7 @@
                         playRtl: false, // If true, the slideshow will move right-to-left
 
                         // Times
-                        delay: 5000, // How long between slideshow transitions in AutoPlay mode (in milliseconds)
+                        delay: 7000, // How long between slideshow transitions in AutoPlay mode (in milliseconds)
                         resumeDelay: 3000, // Resume slideshow after user interaction, only if autoplayLocked is true (in milliseconds).
                         animationTime: 1500, // How long the slideshow transition takes (in milliseconds)
                         delayBeforeAnimate: 0, // How long to pause slide animation before going to the desired slide (used if you want your "out" FX to show).
