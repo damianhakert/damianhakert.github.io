4321af1064c19d8d3018d59d778d38fd058ea554
Job van der Voort <jobvandervoort@gmail.com>
2015-02-11 08:29:45 -0800

    add download partial

--- source/javascripts/mp.js
+++ source/javascripts/mp.js
@@ -9,3 +9,11 @@ mixpanel.track_links("#order-standard", "Clicked Order Standard", {
 mixpanel.track_links("#order-basic", "Clicked Order Basic", {
   "referrer": document.referrer
 });
+
+mixpanel.track_links("#dl-partial", "Clicked download partial", {
+  "referrer": document.referrer
+});
+
+mixpanel.track_links("#dl-partial-sm", "Clicked sm download partial", {
+  "referrer": document.referrer
+});
