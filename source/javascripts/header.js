ae9d07dade7bd2d37a7daf4ee2bc6b0d07eefff0
Job van der Voort <jobvandervoort@gmail.com>
2015-02-16 08:56:52 -0800

    state

--- source/javascripts/header.js
+++ source/javascripts/header.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-  $('.navbar-default .navbar-nav').find('a[href="' + location.pathname + '"]').parents('li').addClass('active');
+  $('.navbar-default .navbar-nav').find('a[href="' + location.pathname + '"]').parents('li').addClass('active-state');
 });
