695c02489248098a8cd4d88c61aeff5099e952a8
Job van der Voort <jobvandervoort@gmail.com>
2015-02-23 18:41:57 -0800

    fix aws page and add ph code to ci page

--- source/javascripts/gitlab-ci.js
+++ source/javascripts/gitlab-ci.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
     $('#signin').addClass('animated bounceIn').addClass('btn-lg btn-default download-btn-style');
     $('#download').addClass('animated bounceIn').addClass('btn-lg btn-default download-btn-style');
+
+
+  if (getUrlParameter('s') == 'ph') {
+    $('#ci-subt').append('<br><br>Hello, Product Hunters!<br><br> Get 75% off a GitLab.com bronze subscription forever! <br> Use the code: producthunt75');
+  }
 });
+
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
