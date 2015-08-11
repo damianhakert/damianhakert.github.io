f8bd6c2906de33c8263a44b90cf45bc752231056
Job van der Voort <jobvandervoort@gmail.com>
2015-08-11 11:19:55 +0200

    remove recurly script completely

--- recurly/start_recurly.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'daemons'
-
-Daemons.run_proc('recurly.rb', {:dir_mode => :normal, :dir => "/opt/recurly/pids", :monitor => true}) do
-  Dir.chdir("/home/deploy/recurly")
-  system "sh -c 'RACK_ENV=production ruby recurly.rb'"
-end
