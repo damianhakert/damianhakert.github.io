1175b265e2a0ea42f8793dcd2b222a507086a8e0
Robert Speicher <rspeicher@gmail.com>
2016-01-19 15:37:45 -0500

    Remove Mixpanel

--- source/javascripts/downloads.js
+++ source/javascripts/downloads.js
@@ -6,8 +6,6 @@ $(document).ready(function(){
   var current = window.location.hash;
 
   $(".dropdown-menu li a").click(function(){
-    mixpanel.track("Clicked download dropdown");
-
     var selText = $(this).text();
     $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
 
