0fe2651f3a524fa40816d696db9d73ddf171c5c9
Phil Hughes <me@iamphill.com>
2016-02-24 20:25:44 +0000

    Added blog Single posts need work

--- config.rb
+++ config.rb
@@ -16,16 +16,46 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
 ###
 # Helpers
 ###
+
+set :haml, {
+  :ugly => true,
+  :format => :html5
+}
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.sources = "posts/{year}-{month}-{day}-{title}"
+  blog.permalink = "{year}/{month}/{day}/{title}/index.html"
+  blog.layout = "post"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -43,7 +73,4 @@ configure :build do
   # activate :minify_javascript
 end
 
-set :haml, {
-  :ugly => true,
-  :format => :html5
-}
+ignore "/includes/*"
---
+++ source/posts/2012-11-13-continuous-integration-server-from-gitlab.markdown
@@ -0,0 +1,46 @@
+---
+layout: post
+title: "Continuous integration server from GitLab"
+date: 2012-11-13 16:00
+comments: true
+categories:
+community: true
+---
+
+### Continuous integration server from GitLab
+
+Hi everyone!
+
+Today I'd like to share a new software crafted by me and Valeriy Sizov. 
+
+Its more than year since we released first version of GitLab. And we dont want to stop on it. 
+
+We wish to have a continuous integration server closely integrated with GitLab.
+
+And today we announced another FOSS software for developers. Its GitLab CI.
+
+![Screenshot](/images/ci_1_0/gitlab_ci_preview.png)
+
+<!-- more -->
+
+[Source Code](https://github.com/gitlabhq/gitlab-ci)
+
+
+**GitLab CI is based on Ruby on Rails and Resque/Redis and**
+
+**GitLab CI supports only git SCM.**
+
+**GitLab CI requires present of GitLab instance with web hooks setup to make builds automatically**
+
+### How it works: 
+
+1. First of all you need to setup GitLab CI on VPS or any linux server
+2. Then you clone projects you want to test and setup test environment
+3. Next step is just add projects to GitLab CI via web UI
+  ![Screenshot](/images/gitlab_ci_new_project.png)
+4. Add just copy HTTP POST url provided by GitLab CI to your GitLab web hooks
+  ![Screenshot](/images/gitlab_ci_project.png)
+5. When you push code to GitLab web hook will trigger CI to make a build
+
+
+[Source Code](https://github.com/gitlabhq/gitlab-ci)
