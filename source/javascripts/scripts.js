9e7e98dd71f6b3d08395b438abb8272df936f679
Phil Hughes <me@iamphill.com>
2016-02-25 09:39:26 +0000

    GitLab CI Moved GitLab CI JS to site-wide scripts file

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,4 +1,21 @@
-$(function() {
-	$("input").not("[type=submit]").jqBootstrapValidation();
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++)
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam)
+    {
+      return sParameterName[1];
+    }
+  }
+}
 
+$(function() {
+  $("input").not("[type=submit]").jqBootstrapValidation();
+
+  var $ci = $('#ci-subt');
+  if (getUrlParameter('s') == 'ph' && $ci.length) {
+    $ci.append('<br><br>Hello, Product Hunters!<br><br> Get 75% off a GitLab.com bronze subscription forever! <br> Use the code: producthunt75');
+  }
 });
