2374a3e8193f16206a35058cfec72d1b41688279
Phil Hughes <me@iamphill.com>
2016-02-26 12:23:12 +0000

    Removed all un-used JS files Some CSS improvements

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,32 +1,58 @@
 function getUrlParameter(sParam) {
   var sPageURL = window.location.search.substring(1);
   var sURLVariables = sPageURL.split('&');
-  for (var i = 0; i < sURLVariables.length; i++)
-  {
+  for (var i = 0; i < sURLVariables.length; i++) {
     var sParameterName = sURLVariables[i].split('=');
-    if (sParameterName[0] == sParam)
-    {
+    if (sParameterName[0] == sParam) {
       return sParameterName[1];
     }
   }
 }
 
-$('#tagline').addClass('animated fadeInLeft');
-$('#hire-us').addClass('animated bounceIn');
 $(function() {
+  var $ci = $('#ci-subt'),
+      $tagLine = $('#tagline'),
+      $hireUs = $('#hire-us'),
+      $tabs = $('#tabs'),
+      $imageLink = $('.image-link');
+
   $("input").not("[type=submit]").jqBootstrapValidation();
 
-  var $ci = $('#ci-subt');
   if (getUrlParameter('s') == 'ph' && $ci.length) {
     $ci.append('<br><br>Hello, Product Hunters!<br><br> Get 75% off a GitLab.com bronze subscription forever! <br> Use the code: producthunt75');
   }
 
   // Consultancy
-  var $tagLine = $('#tagline'),
-      $hireUs = $('#hire-us');
-
   if ($tagLine.length && $hireUs.length) {
     $tagLine.addClass('animated fadeInLeft');
     $hireUs.addClass('animated bounceIn');
+  }
+
+  // Tabs
+  if ($tabs) {
+    var current = window.location.hash;
+
+    $('.tab:not(' + current +')', $tabs).hide();
+
+    $(".dropdown-menu a").on('click', function (e) {
+      e.preventDefault();
+
+      var selText = $(this).text(),
+          current = $(this).attr('href');
+
+      $(this).parents('.btn-group').find('.dropdown-toggle').html(selText +' <span class="caret"></span>');
+
+      $('.tab:not(' + current +')', $tabs).hide();
+      $(current).show();
+      window.location.replace(window.location.origin + window.location.pathname + current);
+
+      $(this).dropdown('toggle');
+    });
+  }
+
+  if ($imageLink.length) {
+    $imageLink.magnificPopup({
+      type:'image'
+    });
   }
 });
