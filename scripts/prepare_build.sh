1ae210df8851c4ff57e16164c511beb36af2faa2
Jacob Vosmaer <jacob@gitlab.com>
2016-06-01 14:35:50 +0200

    Also install xetex

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -f /.dockerinit ]; then
     apt-get update -qq
-    apt-get install -y -qq nodejs pandoc texlive-latex-recommended
+    apt-get install -y -qq nodejs pandoc texlive-latex-recommended texlive-xetex
 fi
