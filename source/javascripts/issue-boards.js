328ec04670d644ea15528270f4506d15bcd030aa
Connor Shea <connor.james.shea@gmail.com>
2016-08-12 14:04:35 -0600

    Make sure variables don't pollute the global namespace.

--- source/javascripts/issue-boards.js
+++ source/javascripts/issue-boards.js
@@ -1,9 +1,9 @@
 $(function () {
   var isElementOnScreen = function($el, scrollTop) {
     // Get very bottom of element
-    elementBottom = $el.offset().top + $el.outerHeight();
+    var elementBottom = $el.offset().top + $el.outerHeight();
     // Get very top of element
-    elementTop = $el.offset().top - scrollTop;
+    var elementTop = $el.offset().top - scrollTop;
     
     if (elementTop <= $(window).height() && elementBottom - scrollTop >= 0) {
       // Element is on-screen
@@ -17,18 +17,18 @@ $(function () {
   $('.js-scroll-to').on('click', function(e) {
     e.preventDefault();
 
-    $target = $(this).attr('href');
+    var $target = $(this).attr('href');
     $('body').animate({
       scrollTop: $target.offset().top
     });
   });
 
   // Scroll effect on steps
-  $steps = $('.js-step, .js-learn-more');
+  var $steps = $('.js-step, .js-learn-more');
 
   $(window).on('scroll', function() {
     $steps.each(function() {
-      isOnScreen = isElementOnScreen($(this), ($(window).scrollTop() - 150));
+      var isOnScreen = isElementOnScreen($(this), ($(window).scrollTop() - 150));
 
       if (isOnScreen && !$(this).hasClass('is-visible')) {
         $(this).addClass('is-visible');
