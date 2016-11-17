72b2c39a45f90750d05a6f98251e8da20d541474
Jose Ivan Vargas Lopez <jvargas@gitlab.com>
2016-11-17 12:31:43 -0600

    Filters now work properly as well as the select list only have one element

--- source/javascripts/events.js
+++ source/javascripts/events.js
@@ -28,7 +28,22 @@
 
       return res;
     };
-}
+  }
+
+  function uniq_fast(a) {
+      var seen = {};
+      var out = [];
+      var len = a.length;
+      var j = 0;
+      for(var i = 0; i < len; i++) {
+           var item = a[i];
+           if(seen[item] !== 1) {
+                 seen[item] = 1;
+                 out[j++] = item;
+           }
+      }
+      return out;
+  }
 
   var setDropdown = function(event){
     var $selected = $(event.currentTarget);
@@ -40,13 +55,71 @@
   }
 
   var filterEvents = function(el){
-    if(el.location !== 'undefined' && el.location !== 'Event Location'){
-      if(this.location === el.location){
+    if(this.location !== 'Event Location'
+       && this.type !== 'Event Type'
+       && this.topic !== 'Event Topic'){
+      if(this.location === el.location
+        && this.type === el.type
+        && this.topic === el.topic){
         return true;
       }
       else{
         return false;
       }
+    } else if(this.location === 'Event Location'
+              && this.type !== 'Event Type'
+              && this.topic !== 'Event Topic'){
+      if(this.type === el.type && this.topic === el.topic){
+        return true;
+      }
+      else{
+        return false;
+      }
+    } else if(this.location !== 'Event Location'
+              && this.type === 'Event Type'
+              && this.topic !== 'Event Topic'){
+      if(this.location === el.location && this.topic === el.topic){
+        return true;
+      }
+      else{
+        return false;
+      }
+    } else if(this.location !== 'Event Location'
+              && this.type !== 'Event Type'
+              && this.topic === 'Event Topic'){
+        if(this.location === el.location && this.type === el.type){
+          return true;
+        }
+        else{
+          return false;
+        }
+    } else if(this.location !== 'Event Location'
+              && this.type === 'Event Type'
+              && this.topic === 'Event Topic'){
+        if(this.location === el.location){
+          return true;
+        }
+        else{
+          return false;
+        }
+    } else if(this.location === 'Event Location'
+              && this.type !== 'Event Type'
+              && this.topic === 'Event Topic'){
+        if(this.type === el.type){
+          return true;
+        }
+        else{
+          return false;
+        }
+    } else if(this.location === 'Event Location'
+              && this.type === 'Event Type'
+              && this.topic !== 'Event Topic'){
+        if(this.topic === el.topic){
+          return true;
+        }
+        else{
+          return false;
+        }
     }
     else{
       return false;
@@ -87,25 +160,22 @@
       return deferred.promise();
     }
 
-    EventsHandler.prototype.renderEventList = function(){
+    EventsHandler.prototype.renderEventList = function(options){
+      console.log('options: ', options);
       var $eventsList = $('.event-list');
-      $eventsList.detach();
-      //Populate the dropdowns
-      var $eventTypeDropdown = $('#event-types ul');
-      var $eventTopicDropdown = $('#event-topics ul');
-      var $eventLocationDropdown = $('#event-location ul');
+      var eventsToPopulate = typeof options !== 'undefined' ? options.filteredData : this.data.events;
+      if(typeof options !== 'undefined'){
+        $eventsList.innerHTML.detach();
+      }
       //The template for the event list
       var $templateEventListElement = $('#events-list-template').html();
       $templateEventListElement = $($templateEventListElement);
-      //The template for the dropdown list
-      var $templateEventListDropdownElement = $('#events-dropdowns-template').html();
-      $templateEventListDropdownElement = $($templateEventListDropdownElement);
       var eventListArray = [];
       var eventTypesArray = [];
       var eventTopicsArray = [];
       var eventLocationArray = [];
       //Create the event list
-      this.data.events.forEach(function(element){
+      eventsToPopulate.forEach(function(element){
         var $tempTemplate = $templateEventListElement.clone()
         $($tempTemplate).find('.event-topic').text(element.topic);
         $($tempTemplate).find('.event-date').text(element.date);
@@ -118,31 +188,17 @@
           $($tempTemplate).find('.social-tags').append('<span class="social-tag">#'+tag+'</span>');
         });
         eventListArray.push($tempTemplate);
-        //Dropdowns, gotta make sure to have only one element and not all included
-        var $tempDropdownEl = $templateEventListDropdownElement.clone();
-        $($tempDropdownEl).append('<span class="value">'+element.type+'</span>');
-        eventTypesArray.push($tempDropdownEl)
-        var $tempDropdownEl1 = $templateEventListDropdownElement.clone();
-        $($tempDropdownEl1).append('<span class="value">'+element.topic+'</span>');
-        eventTopicsArray.push($tempDropdownEl1)
-        var $tempDropdownEl2 = $templateEventListDropdownElement.clone();
-        $($tempDropdownEl2).append('<span class="value">'+element.location+'</span>');
-        eventLocationArray.push($tempDropdownEl2)
       });
       $eventsList.append(eventListArray);
-      $eventTypeDropdown.append(eventTypesArray);
-      $eventTopicDropdown.append(eventTopicsArray);
-      $eventLocationDropdown.append(eventLocationArray);
-      //Set title to the first element of the array
-      $('#event-types .dropdown-title').text('Event Type');
-      $('#event-location .dropdown-title').text('Event Location');
-      $('#event-topics .dropdown-title').text('Event Topic');
-      this.bindEvents();
+
+      if(typeof options === 'undefined'){
+        //Populate the dropdowns
+        this.populateDropdowns();
+      }
+
     }
 
     EventsHandler.prototype.render = function() {
-      var input = this.getFilterValues();
-
       function renderData(shouldWeFilter){
 
         if(shouldWeFilter){
@@ -160,19 +216,67 @@
           renderData.call(this);
         }.bind(this));
       }
+    }
 
+    EventsHandler.prototype.populateDropdowns = function() {
+      var eventTypesDisplayArray = [];
+      var eventTopicsDisplayArray = [];
+      var eventLocationDisplayArray = [];
+      //Populate the dropdowns
+      var $eventTypeDropdown = $('#event-types ul');
+      var $eventTopicDropdown = $('#event-topics ul');
+      var $eventLocationDropdown = $('#event-location ul');
+      //The template for the dropdown list
+      var $templateEventListDropdownElement = $('#events-dropdowns-template').html();
+      $templateEventListDropdownElement = $($templateEventListDropdownElement);
+      var eventTypes = this.data.events.map(function(el){
+        return el.type;
+      });
+      eventTypes = uniq_fast(eventTypes);
+      var eventTopics = this.data.events.map(function(el){
+        return el.topic;
+      });
+      eventTopics = uniq_fast(eventTopics);
+      var eventLocations = this.data.events.map(function(el){
+        return el.location;
+      });
+      eventLocations = uniq_fast(eventLocations);
+      eventTypes.forEach(function(type){
+        var $tempDropdownEl = $templateEventListDropdownElement.clone();
+        $($tempDropdownEl).append('<span class="value">'+type+'</span>');
+        eventTypesDisplayArray.push($tempDropdownEl)
+      });
+      eventTopics.forEach(function(topic){
+        var $tempDropdownEl = $templateEventListDropdownElement.clone();
+        $($tempDropdownEl).append('<span class="value">'+topic+'</span>');
+        eventTopicsDisplayArray.push($tempDropdownEl)
+      });
+      eventLocations.forEach(function(location){
+        var $tempDropdownEl = $templateEventListDropdownElement.clone();
+        $($tempDropdownEl).append('<span class="value">'+location+'</span>');
+        eventLocationDisplayArray.push($tempDropdownEl)
+      });
+      $eventTypeDropdown.append(eventTypesDisplayArray);
+      $eventTopicDropdown.append(eventTopicsDisplayArray);
+      $eventLocationDropdown.append(eventLocationDisplayArray);
+      //Set title to the first element of the array
+      $('#event-types .dropdown-title').text('Event Type');
+      $('#event-location .dropdown-title').text('Event Location');
+      $('#event-topics .dropdown-title').text('Event Topic');
+      this.bindEvents();
     }
 
     EventsHandler.prototype.filterTest = function(){
       var filters = this.getFilterValues();
       var filteredData = this.data.events.filter(filterEvents,filters);
+      this.renderEventList({renderFilteredData: true, filteredData: filteredData});
     }
 
     EventsHandler.prototype.getFilterValues = function(){
       return {
-        type: $('#event-types').find('.dropdown-title').text(),
-        topic: $('#event-topics').find('.dropdown-title').text(),
-        location: $('#event-location').find('.dropdown-title').text()
+        type: $('#event-types').find('.dropdown-title').text() || '',
+        topic: $('#event-topics').find('.dropdown-title').text() || '',
+        location: $('#event-location').find('.dropdown-title').text() || ''
       };
     }
 
