4034087ec7b63113823a68d6b5a7d056d22197a8
Jose Ivan Vargas Lopez <jvargas@gitlab.com>
2016-11-18 16:56:08 -0600

    Pagination and filtering functionality complete.
    Pending:
    -Collapsing event descriptions and animations

--- source/javascripts/events.js
+++ source/javascripts/events.js
@@ -1,48 +1,48 @@
 (function(){
-
-  if (!Array.prototype.filter) {
-    Array.prototype.filter = function(fun/*, thisArg*/) {
-      'use strict';
-
-      if (this === void 0 || this === null) {
-        throw new TypeError();
-      }
-
-      var t = Object(this);
-      var len = t.length >>> 0;
-      if (typeof fun !== 'function') {
-        throw new TypeError();
-      }
-
-      var res = [];
-      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-      for (var i = 0; i < len; i++) {
-        if (i in t) {
-          var val = t[i];
-
-          if (fun.call(thisArg, val, i, t)) {
-            res.push(val);
-          }
-        }
-      }
-
-      return res;
-    };
+  function filterDuplicatesArray(a) {
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for(var i = 0; i < len; i++) {
+      var item = a[i];
+       if(seen[item] !== 1) {
+         seen[item] = 1;
+         out[j++] = item;
+       }
+    }
+    return out;
   }
 
-  function uniq_fast(a) {
-      var seen = {};
-      var out = [];
-      var len = a.length;
-      var j = 0;
-      for(var i = 0; i < len; i++) {
-           var item = a[i];
-           if(seen[item] !== 1) {
-                 seen[item] = 1;
-                 out[j++] = item;
-           }
-      }
-      return out;
+  var getItems = function(total, page){
+    var items = [];
+    if (page > 1) items.push({ title: '<< First', where: 1 });
+    if (page > 1) {
+      items.push({ title: 'Prev', where: page - 1 });
+    } else {
+      items.push({ title: 'Prev', where: page - 1, disabled: true });
+    }
+    if (page > 6) items.push({ title: '...', separator: true });
+
+    const start = Math.max(page - 4, 1);
+    const end = Math.min(page + 4, total);
+
+    for (var i = start; i <= end; i++) {
+      const isActive = i === page;
+      items.push({ title: i, active: isActive, where: i });
+    }
+
+    if (total - page > 4) items.push({ title: '...', separator: true });
+
+    if (page === total) {
+      items.push({ title: 'Next', where: page + 1, disabled: true });
+    } else if (total - page >= 1) {
+      items.push({ title: 'Next', where: page + 1 });
+    }
+
+    if (total - page >= 1) items.push({ title: 'Last ', where: total });
+
+    return items;
   }
 
   var setDropdown = function(event){
@@ -121,14 +121,22 @@
           return false;
         }
     }
+    else if(this.location === 'Event Location'
+              && this.type === 'Event Type'
+              && this.topic === 'Event Topic'){
+      return true;
+    }
     else{
       return false;
     }
   }
 
+  const DESIRED_ELEMENTS_PER_PAGE = 3;
+
   this.EventsHandler = (function(){
     function EventsHandler(){
       this.render();
+      this.currentPage = 1;
     }
 
     EventsHandler.prototype.bindEvents = function(){
@@ -161,56 +169,115 @@
     }
 
     EventsHandler.prototype.renderEventList = function(options){
-      console.log('options: ', options);
       var $eventsList = $('.event-list');
       var eventsToPopulate = typeof options !== 'undefined' ? options.filteredData : this.data.events;
+      if(this.currentPage < 1){
+        this.currentPage = 1;
+        return;
+      }
+      else if(this.currentPage > this.getTotalPages(eventsToPopulate.length)){
+        this.currentPage = this.getTotalPages(eventsToPopulate.length);
+        return;
+      }
       if(typeof options !== 'undefined'){
-        $eventsList.innerHTML.detach();
+        $eventsList.empty();
       }
       //The template for the event list
       var $templateEventListElement = $('#events-list-template').html();
       $templateEventListElement = $($templateEventListElement);
       var eventListArray = [];
-      var eventTypesArray = [];
-      var eventTopicsArray = [];
-      var eventLocationArray = [];
-      //Create the event list
-      eventsToPopulate.forEach(function(element){
-        var $tempTemplate = $templateEventListElement.clone()
-        $($tempTemplate).find('.event-topic').text(element.topic);
-        $($tempTemplate).find('.event-date').text(element.date);
-        $($tempTemplate).find('.event-type').text(element.type);
-        $($tempTemplate).find('.event-location').text(element.location);
-        $($tempTemplate).find('.event-description').text(element.description);
-        //Separate the social tags
-        var socialTags = element.social_tags.trim().split(',');
-        socialTags.forEach(function(tag){
-          $($tempTemplate).find('.social-tags').append('<span class="social-tag">#'+tag+'</span>');
-        });
-        eventListArray.push($tempTemplate);
-      });
-      $eventsList.append(eventListArray);
+      //Unbind all events from the list elements
 
+      $('.pagination li span').off('click');
+      if(eventsToPopulate.length > DESIRED_ELEMENTS_PER_PAGE){
+        document.querySelector('.pagination').innerHTML =
+        getItems(this.getTotalPages(eventsToPopulate.length), this.currentPage)
+        .map(function(e, i){
+          return('<li seperator="'+e.seperator+'" active="'+e.active+'" disabled="'+e.disabled+'">'
+          + '<span>'+e.title+'</span>'
+          + '</li>');
+        }).join('');
+        $('.pagination li[active="false"] span').on('click', this.render.bind(this));
+        $('.pagination li[active="undefined"] span').on('click', this.render.bind(this));
+      }
+      else{
+        document.querySelector('.pagination').innerHTML = '';
+      }
+
+      //Create the event list based on the data that we have filtered and page
+      for(var i = (this.currentPage - 1) * DESIRED_ELEMENTS_PER_PAGE;
+      i < (this.currentPage * DESIRED_ELEMENTS_PER_PAGE); i += 1){
+        var element = eventsToPopulate[i];
+        if(element){
+          var $tempTemplate = $templateEventListElement.clone()
+          $($tempTemplate).find('.event-topic').append('<span class="fa fa-chevron-down" aria-hidden="true"></span>'+element.topic);
+          $($tempTemplate).find('.event-date').text(element.date);
+          $($tempTemplate).find('.event-type').text(element.type);
+          $($tempTemplate).find('.event-location').text(element.location);
+          $($tempTemplate).find('.event-description').text(element.description);
+          //Separate the social tags
+          var socialTags = element.social_tags.trim().split(',');
+          socialTags.forEach(function(tag){
+            $($tempTemplate).find('.social-tags').append('<span class="social-tag">#'+tag+'</span>');
+          });
+          eventListArray.push($tempTemplate);
+        }
+        else{
+          i = (this.currentPage * DESIRED_ELEMENTS_PER_PAGE) + 1;
+        }
+      }
+      $eventsList.append(eventListArray);
       if(typeof options === 'undefined'){
         //Populate the dropdowns
         this.populateDropdowns();
       }
-
     }
 
-    EventsHandler.prototype.render = function() {
-      function renderData(shouldWeFilter){
 
-        if(shouldWeFilter){
-          this.filterTest();
+    EventsHandler.prototype.render = function(event) {
+      function renderData(shouldWeFilter, pageNumber){
+        if(shouldWeFilter && !pageNumber){
+          var filters = this.getFilterValues();
+          var filteredData = this.data.events.filter(filterEvents,filters);
+          this.currentPage = 1;
+          this.renderEventList({renderFilteredData: true, filteredData: filteredData});
+        }
+        else if(shouldWeFilter && pageNumber){
+          var filters = this.getFilterValues();
+          var filteredData = this.data.events.filter(filterEvents,filters);
+          var newPageNumber;
+          if(pageNumber.text().toLowerCase() === 'next'){
+            newPageNumber = this.currentPage += 1;
+          }
+          else if(pageNumber.text().toLowerCase() === 'prev'){
+            newPageNumber = this.currentPage -= 1;
+          }
+          else if(pageNumber.text().toLowerCase().indexOf('first') !== -1){
+            newPageNumber = this.currentPage = 1;
+          }
+          else if(pageNumber.text().toLowerCase().indexOf('last') !== -1){
+            newPageNumber = this.getTotalPages(filteredData.length);
+          }
+          else{
+            newPageNumber = parseInt(pageNumber.text());
+          }
+          this.currentPage = newPageNumber;
+          this.renderEventList({renderFilteredData: true, filteredData: filteredData});
         }
         else{
           this.renderEventList();
         }
       }
+      if(this.data && typeof event !== 'undefined'){
+        var $callingElement = $(event.currentTarget);
+        var classCallingElement = $callingElement.parent()[0].className;
+        if(classCallingElement.indexOf('dropdown-menu') != -1){
+          renderData.call(this, true);
+        }
+        else{
+          renderData.call(this, true, $callingElement);
+        }
 
-      if(this.data){
-        renderData.call(this, true);
       } else {
         $.when(this.getData()).then(function(){
           renderData.call(this);
@@ -229,18 +296,21 @@
       //The template for the dropdown list
       var $templateEventListDropdownElement = $('#events-dropdowns-template').html();
       $templateEventListDropdownElement = $($templateEventListDropdownElement);
-      var eventTypes = this.data.events.map(function(el){
+      eventTypes = this.data.events.map(function(el){
         return el.type;
       });
-      eventTypes = uniq_fast(eventTypes);
-      var eventTopics = this.data.events.map(function(el){
+      eventTypes.unshift('Event Type');
+      eventTypes = filterDuplicatesArray(eventTypes);
+      eventTopics = this.data.events.map(function(el){
         return el.topic;
       });
-      eventTopics = uniq_fast(eventTopics);
+      eventTopics.unshift('Event Topic');
+      eventTopics = filterDuplicatesArray(eventTopics);
       var eventLocations = this.data.events.map(function(el){
         return el.location;
       });
-      eventLocations = uniq_fast(eventLocations);
+      eventLocations.unshift('Event Topic');
+      eventLocations = filterDuplicatesArray(eventLocations);
       eventTypes.forEach(function(type){
         var $tempDropdownEl = $templateEventListDropdownElement.clone();
         $($tempDropdownEl).append('<span class="value">'+type+'</span>');
@@ -278,6 +348,10 @@
         topic: $('#event-topics').find('.dropdown-title').text() || '',
         location: $('#event-location').find('.dropdown-title').text() || ''
       };
+    }
+
+    EventsHandler.prototype.getTotalPages = function(dataLength){
+      return Math.ceil(dataLength / DESIRED_ELEMENTS_PER_PAGE);
     }
 
     return EventsHandler;
