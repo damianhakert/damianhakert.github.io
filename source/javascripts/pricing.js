759e7a66ca850755d7dad05974bf4a559d51f984
Kushal Pandya <kushal@gitlab.com>
2016-12-07 19:35:40 +0530

    Add event listener to expand/collapse edition card info

--- source/javascripts/pricing.js
+++ source/javascripts/pricing.js
@@ -30,6 +30,21 @@ $(function () {
   setTimeout(equilize, 500);
   $(window).on('resize', equilize);
 
+  $('.featured-item-expand').on('click', function(e) {
+    var listExpander = $(this);
+        targetFeatureList = $($(this).attr('href'));
+
+    if (!targetFeatureList.is(':visible')) {
+      targetFeatureList.addClass('list-expanded');
+      listExpander.addClass('chevron-up');
+    } else {
+      targetFeatureList.removeClass('list-expanded');
+      listExpander.removeClass('chevron-up');
+    }
+    
+    e.preventDefault();
+  });
+
   $('.js-faq-question').on('click', function (e) {
     e.preventDefault();
     isOpen = $(this).closest('.faq-item').is('.is-open');
