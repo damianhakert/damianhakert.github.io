f4e359f07280060eb2cc8f73928bab00a404e176
José Iván Vargas López <jvargas@gitlab.com>
2016-12-16 08:06:29 -0600

    Added keyup events, modified the size of the search bar
    Adjusted z-index for the search suggestions
    When selecting a suggestion the suggestion box hides

--- source/javascripts/applications.js
+++ source/javascripts/applications.js
@@ -1,30 +1,83 @@
-(function(){
+(function($){
 
-  var scrollToElement = function(e, suggestion){
-    var container = $('body'),
-        scrollTo = $('h4:contains("'+suggestion+'")');
+  const MAX_RESULTS = 5;
 
+  var scrollToElement = function() {
+    var container = $('body');
+        scrollTo = $('h4:icontains("'+this.searchContainer.val()+'")');
     container.animate({
         scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop() - 50
     });
+    this.suggestions.html('');
   }
 
-  var findMatches = function(searchQuery, apps){
+  var findMatches = function(searchQuery, apps) {
     return apps.filter(function (app){
       var regex = new RegExp(searchQuery, 'gi');
       return app.match(regex);
     });
   }
 
-  var displayMatches = function(){
-    const matches = findMatches(this.searchContainer.val(), this.data.applicationTitles);
-    
+  var displayMatches = function(e) {
+    e.preventDefault();
+    if(e.type === 'keyup'){
+      if(e.keyCode === 38 || e.keyCode === 40) {
+        var selectedSuggestion = $(this.suggestions).find('.selected-suggestion');
+        if(!selectedSuggestion.length) {
+          if(e.keyCode === 40) {
+            var nextEl = $(this.suggestionsListElements)[0];
+            $(nextEl).addClass('selected-suggestion');
+          }
+        }
+        else {
+          selectedSuggestion.removeClass('selected-suggestion');
+          if(e.keyCode === 40) {
+            var nextEl = selectedSuggestion.next();
+            if (!nextEl.length) {
+              nextEl = $(this.suggestionsListElements)[0];
+            }
+            $(nextEl).addClass('selected-suggestion');
+          } else { //keydown
+            var prev = selectedSuggestion.prev();
+            if (!prev.length) {
+              prev = $(this.suggestionsListElements).last();
+            }
+            prev.addClass('selected-suggestion');
+          }
+        }
+      } else if(e.keyCode === 13) {
+        this.searchContainer.val($('.selected-suggestion').text());
+        scrollToElement.call(this);
+      } else {
+        if(this.searchContainer.val() !== '') {
+          var matches = findMatches(this.searchContainer.val(), this.data.applicationTitles);
+          var that = this;
+          matches = matches.length > MAX_RESULTS ? matches.slice(0, MAX_RESULTS) : matches;
+          var html = matches.map(function (app){
+            var regex = new RegExp(that.searchContainer.val(), 'gi');
+            var application = app.replace(regex, '<span class="hl">'+that.searchContainer.val()+'</span>');
+            return '<li>'+application+'</li>'
+          }).join('');
+          this.suggestions.html(html);
+          this.suggestionsListElements = $('.suggestions li');
+          this.suggestionsListElements.on('click', function(e) {
+            var target = $(e.target);
+            that.searchContainer.val(target.text());
+            that.suggestions.html('');
+            scrollToElement.call(that);
+          });
+        } else {
+          this.suggestions.html('');
+        }
+      }
+    }
   }
 
-  this.ApplicationSearchBar = (function(){
+  this.ApplicationSearchBar = (function() {
     function ApplicationSearchBar(){
       this.searchContainer = $('.search-apps');
-      this.suggestions = $();
+      this.suggestions = $('.suggestions');
+      this.suggestionsListElements = $('.suggestions li');
       this.initializeSearch();
     }
 
@@ -39,9 +92,12 @@
       return deferred.promise();
     }
 
-    ApplicationSearchBar.prototype.bindEvents = function(){
+    ApplicationSearchBar.prototype.bindEvents = function() {
       this.searchContainer.bind('change', displayMatches.bind(this));
       this.searchContainer.bind('keyup', displayMatches.bind(this));
+      this.searchContainer.bind('click', function (){
+        console.log(this);
+      });
     }
 
     ApplicationSearchBar.prototype.initializeSearch = function() {
@@ -59,10 +115,15 @@
       }
     }
 
+    jQuery.expr[':'].icontains = function(a, i, m) {
+      return jQuery(a).text().toLowerCase()
+        .indexOf(m[3].toLowerCase()) >= 0;
+    };
+
     return ApplicationSearchBar;
 
   })();
 
   new ApplicationSearchBar();
 
-})();
+})(jQuery);
