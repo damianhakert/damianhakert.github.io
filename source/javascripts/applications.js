ee49696121fe110288686aed3b90aa96071c6b89
Jose Ivan Vargas <jvargas@gitlab.com>
2017-01-05 12:05:16 -0600

    Removed the getData() function along more JS improvements

--- source/javascripts/applications.js
+++ source/javascripts/applications.js
@@ -2,13 +2,27 @@
 
   const MAX_RESULTS = 5;
 
+  var filterDuplicatesArray = function(array) {
+    var seen = {};
+    var out = [];
+    var j = 0;
+    for(var i = 0; i < array.length; i++) {
+      var item = array[i];
+       if(seen[item] !== 1) {
+         seen[item] = 1;
+         out[j++] = item;
+       }
+    }
+    return out;
+  }
+
   var scrollToElement = function() {
     var container = $('html, body');
         scrollTo = $('h4:icontains("'+this.searchContainer.val()+'")');
     container.animate({
         scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop() - 180
     });
-    this.suggestions.html('');
+    this.suggestions.empty();
     this.searchContainer.blur();
   }
 
@@ -26,7 +40,7 @@
         $(this).show();
       });
       $(this).next('.row').find('h4').each(function() {
-        $(this).parent().show();
+        $(this).closest('.col-md-4').show();
       });
       $(this).prev('hr').show();
     });
@@ -34,6 +48,7 @@
 
   var showAppsAccordingToSearch = function(searchQuery) {
     var regex = new RegExp(searchQuery, 'gi');
+    var allAppsHidden = true;
     this.appListDOM.each(function () {
       var appsInCategory = $(this).next('.row').find('h4');
       var appCategoryRows = $(this).next('.row');
@@ -42,9 +57,10 @@
       appsInCategory.each(function() {
         if($(this).text().match(regex)) {
           hideCategoryTitle = false;
-          $(this).parent().show();
+          allAppsHidden = false;
+          $(this).closest('.col-md-4').show();
         } else {
-          $(this).parent().hide();
+          $(this).closest('.col-md-4').hide();
         }
       });
       if(hideCategoryTitle) {
@@ -55,6 +71,9 @@
         });
       }
     });
+    if(allAppsHidden) {
+      showAllApps.call(this);
+    }
   }
 
   var populateList = function(searchQuery, apps) {
@@ -71,7 +90,7 @@
       e.stopPropagation();
       var target = $(e.target);
       this.searchContainer.val(target.text());
-      this.suggestions.html('');
+      this.suggestions.empty();
       showAppsAccordingToSearch.call(this, this.searchContainer.val());
       scrollToElement.call(this);
     }.bind(this));
@@ -115,26 +134,26 @@
           scrollToElement.call(this);
         }
       } else if (e.keyCode === 27) {
-        this.suggestions.html('');
-      } else if(e.keyCode === 8) {
+        this.suggestions.empty();
+      } else if(e.keyCode === 8 || e.keyCode === 46) {
         if(this.searchContainer.val() === '') {
           this.searchIcon.show();
           this.resetIcon.hide();
           showAllApps.call(this);
-          this.suggestions.html('');
+          this.suggestions.empty();
         }
       } else {
         if(this.searchContainer.val() !== '') {
-          populateList.call(this, this.searchContainer.val(), this.data.applicationTitles);
+          populateList.call(this, this.searchContainer.val(), this.applicationTitles);
           showAppsAccordingToSearch.call(this, this.searchContainer.val());
           this.searchIcon.hide();
           this.resetIcon.show();
         } else {
-          this.suggestions.html('');
+          this.suggestions.empty();
         }
       }
     } else if(e.type === 'focus' && this.searchContainer.val() !== '') {
-      populateList.call(this, this.searchContainer.val(), this.data.applicationTitles);
+      populateList.call(this, this.searchContainer.val(), this.applicationTitles);
       this.searchIcon.hide();
       this.resetIcon.show();
     }
@@ -149,18 +168,12 @@
       this.resetIcon = $('.reset');
       this.resetIcon.hide();
       this.appListDOM = $('.app-list').find('h2');
-      this.initializeSearch();
-    }
-
-    ApplicationSearchBar.prototype.getData = function() {
-      var deferred = jQuery.Deferred();
-
-      $.get('/applications/data.json').then(function(data) {
-        this.data = data;
-        deferred.resolve();
-      }.bind(this));
-
-      return deferred.promise();
+      var applicationTitles = [];
+      $('.app-list').find('h4').each(function() {
+        applicationTitles.push($(this).text().trim());
+      });
+      this.applicationTitles = filterDuplicatesArray(applicationTitles);
+      this.bindEvents();
     }
 
     ApplicationSearchBar.prototype.bindEvents = function() {
@@ -174,28 +187,13 @@
     ApplicationSearchBar.prototype.resetSearch = function(e) {
       var $target = $(e.target);
       if(!$target.hasClass('search-apps')) {
-        this.suggestions.html('');
+        this.suggestions.empty();
       }
       if($target.hasClass('reset')) {
         this.searchContainer.val('');
         this.searchIcon.show();
         this.resetIcon.hide();
         showAllApps.call(this);
-      }
-    }
-
-    ApplicationSearchBar.prototype.initializeSearch = function() {
-
-      function startHound(){
-        this.bindEvents();
-      }
-
-      if(this.data) {
-        startHound.call(this);
-      } else {
-        $.when(this.getData()).then(function() {
-          startHound.call(this);
-        }.bind(this));
       }
     }
 
