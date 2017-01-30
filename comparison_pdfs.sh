a6ceb6b07fd84fe9dba1a2f47a5c7d4fd5006062
Phil Hughes <me@iamphill.com>
2017-01-30 16:01:07 +0000

    Slight styling improvements

--- comparison_pdfs.sh
+++ comparison_pdfs.sh
@@ -1,1 +1,1 @@
-wkhtmltopdf --margin-left 0 --margin-right 0 --margin-top 0 public/comparison/pdfs/gitlab-ce-ee-vs-bitbucket-server.html test.pdf
+wkhtmltopdf --footer-html public/comparison/pdfs/footer_html.html --margin-left 0 --margin-right 0 --margin-top 0 public/comparison/pdfs/gitlab-ce-ee-vs-bitbucket-server.html test.pdf
