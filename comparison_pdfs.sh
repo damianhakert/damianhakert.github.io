d4361aa7f403c8cf3b73995af10e3a34399a5d0a
Phil Hughes <me@iamphill.com>
2017-02-09 12:32:33 +0000

    Changed how the pages are generated

--- comparison_pdfs.sh
+++ comparison_pdfs.sh
@@ -1,1 +1,12 @@
-wkhtmltopdf --footer-html public/comparison/pdfs/footer_html.html --margin-left 0 --margin-right 0 --margin-top 0 public/comparison/pdfs/gitlab-ce-ee-vs-bitbucket-server.html test.pdf
+#!/bin/bash
+for filename in public/comparison/pdfs/*.html; do
+  pdffilename=${filename/.html/.pdf}
+  echo "👉  Generating PDF for $filename..."
+
+  wkhtmltopdf --margin-left 0 --margin-right 0 --margin-top 0 --zoom 3 $filename $pdffilename
+
+  # remove the HTML file
+  # rm $filename
+
+  echo "✅  PDF for $filename generated!"
+done
